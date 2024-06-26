f229f0a22f4250ccb913243aa0f9b3facdd715e3
jmmarcuis <marcosjhainnoallrick@gmail.com>
2024-06-26 13:45:48 +0800

    added something

---
+++ Madgoodies/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+
+namespace Madgoodies.Controllers
+{
+    public class LoginController
+    {
+    }
+}
