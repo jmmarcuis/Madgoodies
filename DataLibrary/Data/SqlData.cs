e7b255dcd9e66a52c456c5b82931bfb9de93e0b1
hanni <plsvega@mymail.mapua.edu.ph>
2024-06-26 12:54:11 +0800

    some backend

---
+++ DataLibrary/Data/SqlData.cs
@@ -0,0 +1,43 @@
+﻿using BlogDataLibrary.Database;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+
+namespace DataLibrary.Data
+{
+    public class SqlData
+    {
+  
+        private readonly ISqlDataAccess _db;
+        private const string connectionStringName = "SqlDb";
+
+        public SqlData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+
+        public UserModel Authenticate(
+            string username,
+            string password)
+        {
+
+            UserModel result = _db.LoadData<UserModel, dynamic>("dbo.spUsers_Authenticate",
+                new { username, password },
+                connectionStringName,
+                true).FirstOrDefault();
+
+            return result;
+        }
+
+
+
+
+    }
+}
---
+++ MadgoodiesDB/dbo/Tables/Goods.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Goods]
+(
+  ProductID INT NOT NULL PRIMARY KEY,
+  [ProductName] VARCHAR(100) NOT NULL,
+  Price DECIMAL(10,2) NOT NULL,
+  Stock INT NOT NULL,
+  [Description] TEXT NOT NULL
+)
